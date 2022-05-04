c57cb44c717ce3567c5e95d2154c966a3938d240
tealbox <96038779+tealbox@users.noreply.github.com>
2022-05-04 17:22:06 +0530

    Update apic-env.py

--- apic-env.py
+++ apic-env.py
@@ -1,11 +1,14 @@
 #-------------------------------------------------------------------------------
 # Name:        Apic-Env-Setup
 #-------------------------------------------------------------------------------
-## 
-## issue with SSL_V3, 
+## Note: only for Python2
+## issue with SSL_V3, _ssl.c:510: error:14077410:SSL routines:SSL23_GET_SERVER_HELLO:sslv3 alert handshake failure>
+# This can;t be resolve in python2 of urllib, but it require higher version of urllib may be 3
+# so I downloaded python3 and used for retriving Pyscripter
+# no error handling, only for personal use/not commercial 
 
 import os, zipfile
-import urllib.request
+import urllib
 path = 'c:\\myTools'
 
 def getPython(url=None):
@@ -13,12 +16,13 @@ def getPython(url=None):
         url = "https://www.python.org/ftp/python/3.10.2/python-3.10.2-embed-amd64.zip"
     pfile = "python-3.10.2-embed-amd64.zip"
     os.chdir(path)
-    urllib.request.urlretrieve(url,pfile)
+    urllib.urlretrieve(url,pfile)
     zip_file = zipfile.ZipFile(pfile)
     zip_file.extractall()
 
 def getScripter(url=None):
-##    os.system(''' c:\\mytools\\python.exe -c "import os ; os.system('mkdir TiTi'); " ''')
+## os.system(''' c:\\mytools\\python.exe -c "import os ; os.system('mkdir ToDo'); " ''')
+## 
     if url is None:
         url = "https://sourceforge.net/projects/pyscripter/files/PyScripter-v4.1/PyScripter-4.1.1-x64.zip/download"
     pfile = "pyscripter.zip"
@@ -37,15 +41,15 @@ def getTopMost(url=None):
         url = "http://www.nirsoft.net/utils/winlister-x64.zip"
     pfile = "winlister.zip"
     os.chdir(path)
-    urllib.request.urlretrieve(url,pfile)
+    urllib.urlretrieve(url,pfile)
     zip_file = zipfile.ZipFile(pfile)
     zip_file.extractall()
 
 def main():
-##    os.mkdir(path)
+    os.mkdir(path)
     os.chdir(path)
-##    getPython()
-##    getScripter()
+    getPython()
+    getScripter()
     getTopMost()
     pass
 
